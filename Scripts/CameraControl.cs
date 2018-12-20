7addb5d52f7cc23c3aadd608acc8c70350b151b5
qmsiteandy <ht93084@gmail.com>
2018-12-20 15:42:48 +0800

    註解、更新
    加入一些程式註解
    並把scene、cameraHolder、fruitmenManager加入prefabs

--- Scripts/CameraControl.cs
+++ Scripts/CameraControl.cs
@@ -4,51 +4,60 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
-    public Transform target;
-    public float smoothSpeed = 0.05f;
-    public float lookUpMove = 1.5f;
-    public float lookDownMove = 4f;
+    public Transform target;            //camera追蹤目標
+    public float smoothSpeed = 0.05f;   //camera移動速度
+    public float lookUpMove = 1.5f;     //向上看時camera垂直移動距離
+    public float lookDownMove = 4f;     //向下看時camera垂直移動距離
 
-    private Vector3 offset;
-    private Vector3 AimPos;
-    bool isFollowMode = true;
+    private Vector3 offset;             //camera與主角相對位置差距
+    private Vector3 AimPos;             //camera目標前往位置
+    bool isFollowMode = true;           //是否為一般跟隨模式
     
-	// Use this for initialization
-	void Start () {
-        offset = transform.position - target.position;
+
+	void Start ()
+    {
+        //設定offset為初始主角及初始相機位置之差距
+        offset = transform.position - target.position; 
 	}
 	
-	// Update is called once per frame
-	void FixedUpdate () {
-
+	void FixedUpdate ()
+    {
+        //如果是followMode，設定camera目標前往位置
         if (isFollowMode) { AimPos = target.position + offset; }   
 
+        //平滑移動camera
         Vector3 smoothPos = Vector3.Lerp(transform.position, AimPos, smoothSpeed);
         transform.position = smoothPos;
     }
 
-
+    //主角往上看時呼叫此函式
     public void PlayerLookUp()
     {
+        //設定目標前往位置往上移
         AimPos += new Vector3(0f, lookUpMove, 0f);
+        //停止follow
         isFollowMode = false;
     }
 
-
+    //主角往下看時呼叫此函式
     public void PlayerLookDown()
     {
+        //設定目標前往位置往下移
         AimPos -= new Vector3(0f, lookDownMove, 0f);
+        //停止follow
         isFollowMode = false;
     }
 
-
+    //設定camera目標位置並停止follow主角
     public void SetAimPos(float xAim, float yAim)
     {
+        //設定camera目標位置
         AimPos = new Vector3(xAim, yAim, 0f);
+        //停止follow
         isFollowMode = false;
     }
 
-
+    //回歸follow主角
     public void BackNormal()
     {
         isFollowMode = true;
--- Scripts/CameraShake.cs
+++ Scripts/CameraShake.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour {
 
-	// Use this for initialization
+    //Shake函式，引數為震動長度及震動幅度
 	public IEnumerator Shake(float duration,float magnitude)
     {
+        //儲存原始camera位置
         Vector3 originalPos = transform.localPosition;
 
+        //已經過的時間(計時器)
         float elapsed = 0f;
 
         while (elapsed < duration)
         {
+            //震動位置
             float x = Random.Range(-1, 1) * magnitude;
             float y = Random.Range(-1, 1) * magnitude;
 
+            //設定camera位置
             transform.localPosition = new Vector3(x, y, originalPos.z);
 
+            //計時
             elapsed += Time.deltaTime;
 
+            //每偵跳脫
             yield return null;
         }
+        //回到camera起始位置
         transform.localPosition = originalPos;
     }
 }
