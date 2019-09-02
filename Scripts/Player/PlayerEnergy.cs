e0ebd76f5d97c731ad3eeeeb00b9da5e34089263
Remon <ht93084@gmail.com>
2019-09-02 21:07:26 +0800

    1080902主角變髒shader material設定 ver.1
    未來希望再多一些外光暈及粒子特效

--- Scripts/Player/PlayerEnergy.cs
+++ Scripts/Player/PlayerEnergy.cs
@@ -13,7 +13,9 @@ public class PlayerEnergy : MonoBehaviour {
     public int waterPerCharge = 3;
     private float elapsed = 0f;
 
-    private SpriteRenderer[] playerSprite= { null, null, null };
+    //private SpriteRenderer[] playerSprite= { null, null, null };
+    private GameObject[] Oka = { null, null, null };
+    public Material OkaMat;
 
     private UI_Manager UI_manager;
 
@@ -23,8 +25,11 @@ public class PlayerEnergy : MonoBehaviour {
         waterEnergy = waterEnergyMax;
         dirtMax = waterEnergyMax;
 
-        for (int x = 0; x < 3; x++) playerSprite[x] = transform.GetChild(x).GetComponent<SpriteRenderer>();
+        //for (int x = 0; x < 3; x++) playerSprite[x] = transform.GetChild(x).GetComponent<SpriteRenderer>();
+        //for (int x = 0; x < 3; x++) Oka[x] = transform.GetChild(x).gameObject;
         UI_manager = GameObject.Find("UI_Canvas").GetComponent<UI_Manager>();
+
+        OkaMat.SetFloat("Vector1_1381CB45", 0f);
     }
 
     void Update()
@@ -62,5 +67,7 @@ public class PlayerEnergy : MonoBehaviour {
         float dirtyDegree;
         dirtyDegree = (float)dirt / (dirt + waterEnergy);
         UI_manager.SetDirtyUI(dirtyDegree);
+
+        OkaMat.SetFloat("Vector1_1381CB45", dirtyDegree);
     }
 }
